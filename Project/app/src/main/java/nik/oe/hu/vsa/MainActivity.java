8d7b8a3ecf66e42b932d555722a486ce5508cac7
Bakonyi Gergő <bakonyi.gergo.istvan@gmail.com>
2018-05-11 17:17:25 +0200

    asd

--- Project/app/src/main/java/nik/oe/hu/vsa/MainActivity.java
+++ Project/app/src/main/java/nik/oe/hu/vsa/MainActivity.java
@@ -52,6 +52,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        this.createFakeInstances();
+
     }
 
     @Override
